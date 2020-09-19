d13ee742a461a38f9063ce4d491b9614e67cdaba
Arnastria <arnas.bgr@gmail.com>
2020-09-19 10:03:53 +0700

    new addition json

--- server/server.js
+++ server/server.js
@@ -1,10 +1,30 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+const fs = require('fs')
 const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, '..', 'build');
+const srcPath = path.join(__dirname, '..', 'src');
 
 app.use(express.static(publicPath));
+
+app.get('/src/data', (req, res) => {
+
+    fs.readFile(path.join(srcPath, 'data/data.json'), 'utf8', (err, jsonString) => {
+        if (err) {
+            console.log("Error reading file from disk:", err)
+            return
+        }
+        try {
+            const data = JSON.parse(jsonString)
+            console.log("Name :", data.name)
+            res.send(data);
+
+        } catch (err) {
+            console.log('Error parsing JSON string:', err)
+        }
+    })
+});
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(publicPath, 'index.html'));
